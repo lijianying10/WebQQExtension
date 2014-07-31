13b7f2af9050bcb0959a8f999dc40123b8691dd7
lijianying10 <lijianying12@gmail.com>
2014-07-31 10:05:28 +0800

    post test success

--- chrome_extension/contentscript.js
+++ chrome_extension/contentscript.js
@@ -1,10 +1,10 @@
 console.log("xss test");
+// // How to use XHR get
 // var xhr = new XMLHttpRequest();
 // xhr.open("GET", "http://www.baidu.com", true);
 // xhr.onreadystatechange = function() {
 //   console.log(xhr.readyState);
 //   if (xhr.readyState == 4) {
-//     // // 警告! 这里有可能执行了一段恶意脚本!
 //     // var resp = eval("(" + xhr.responseText + ")");
 //     // ...
 //     console.log( xhr.responseText);
@@ -12,8 +12,52 @@ console.log("xss test");
 // }
 // xhr.send();
 
+
+//convert json object to url query
+// example obj_to_query({abc:'123',kkk:'bbb'}); it will out put "?abc=123&kkk=bbb"
+function obj_to_query(obj) {
+    var parts = [];
+    for (var key in obj) {
+        if (obj.hasOwnProperty(key)) {
+            parts.push(encodeURIComponent(key) + '=' + encodeURIComponent(obj[key]));
+        }
+    }
+    return parts.join('&');
+}
+
+
+
+
+
+var postUrl = 'http://127.0.0.1:8080/posttest';
+var params = obj_to_query({title:'test_title',kkk:'test param2'});
+var xhr = new XMLHttpRequest();
+xhr.open('POST', postUrl, true);
+xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+xhr.onreadystatechange = function() { 
+  console.log(xhr.readyState);
+  console.log(xhr.status);
+  console.log(xhr.responseText);
+  if (xhr.readyState == 4 && xhr.status == 200) {
+    console.log(xhr.responseText);
+  }
+};
+xhr.send(params);
+
+
+
+
+
 //appendchild 用法
 // document.getElementsByClassName('suggestion')[0].appendChild(document.getElementsByClassName('text_ellipsis user_nick')[0]);
+
+
+
+
+
+
+
+
 
 function loadUI(){
   if(document.readyState!="complete")
--- wqq/main.py
+++ wqq/main.py
@@ -1,17 +1,90 @@
-import web
+import re
+from cgi import escape,parse_qs
 
-urls = (
-    '/', 'index'
-)
+def index(environ, start_response):
+    """This function will be mounted on "/" and display a link
+    to the hello world page."""
+    start_response('200 OK', [('Content-Type', 'text/html')])
+    return ['''Hello World Application
+               This is the Hello World application:
 
-class index:
-    def GET(self):
-        return "Hello, world!"
-    def POST(self):
-        i = web.input()
-        n = db.insert('todo', title=i.title)
-        raise web.seeother('/')
+`continue <hello/>`_
 
-if __name__ == "__main__":
-    app = web.application(urls, globals())
-    app.run()
+''']
+
+def hello(environ, start_response):
+    """Like the example above, but it uses the name specified in the
+URL."""
+    # get the name from the url if it was specified there.
+    args = environ['myapp.url_args']
+    if args:
+        subject = escape(args[0])
+    else:
+        subject = 'World'
+    start_response('200 OK', [('Content-Type', 'text/html')])
+    return ['''Hello %(subject)s
+            Hello %(subject)s!
+
+''' % {'subject': subject}]
+
+def not_found(environ, start_response):
+    """Called if no URL matches."""
+    start_response('404 NOT FOUND', [('Content-Type', 'text/plain')])
+    return ['Not Found']
+
+def posttest(environ, start_response):
+   try:
+      request_body_size = int(environ.get('CONTENT_LENGTH', 0))
+   except (ValueError):
+      request_body_size = 0
+   request_body = environ['wsgi.input'].read(request_body_size)
+   d = parse_qs(request_body)
+
+   # age = d.get('age', [''])[0] # Returns the first age value.
+   # hobbies = d.get('hobbies', []) # Returns a list of hobbies.
+   #
+   # # Always escape user input to avoid script injection
+   # age = escape(age)
+   # hobbies = [escape(hobby) for hobby in hobbies]
+   print d
+   title = d.get('title', [''])[0]
+   response_body = title
+
+   status = '200 OK'
+
+   response_headers = [('Content-Type', 'text/html'),
+                  ('Content-Length', str(len(response_body)))]
+   start_response(status, response_headers)
+
+   return [response_body]
+
+# map urls to functions
+urls = [
+    (r'^$', index),
+    (r'hello/?$', hello),
+    (r'hello/(.+)$', hello),
+    (r'posttest', posttest)
+]
+
+def application(environ, start_response):
+    """
+    The main WSGI application. Dispatch the current request to
+    the functions from above and store the regular expression
+    captures in the WSGI environment as  `myapp.url_args` so that
+    the functions from above can access the url placeholders.
+
+    If nothing matches call the `not_found` function.
+    """
+    path = environ.get('PATH_INFO', '').lstrip('/')
+    for regex, callback in urls:
+        match = re.search(regex, path)
+        if match is not None:
+            environ['myapp.url_args'] = match.groups()
+            return callback(environ, start_response)
+    return not_found(environ, start_response)
+
+if __name__ == '__main__':
+    from wsgiref.simple_server import make_server
+    srv = make_server('localhost', 8080, application)
+    print 'server running at : http://127.0.0.1:8080/'
+    srv.serve_forever()
