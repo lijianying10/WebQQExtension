c88d35cc03588a8f16b3f2d62794b8dc2f5cbab4
lijianying10 <lijianying12@gmail.com>
2014-07-31 10:29:55 +0800

    change wall paper

--- chrome_extension/contentscript.js
+++ chrome_extension/contentscript.js
@@ -15,34 +15,34 @@ console.log("xss test");
 
 //convert json object to url query
 // example obj_to_query({abc:'123',kkk:'bbb'}); it will out put "?abc=123&kkk=bbb"
-function obj_to_query(obj) {
-    var parts = [];
-    for (var key in obj) {
-        if (obj.hasOwnProperty(key)) {
-            parts.push(encodeURIComponent(key) + '=' + encodeURIComponent(obj[key]));
-        }
-    }
-    return parts.join('&');
-}
+// function obj_to_query(obj) {
+//     var parts = [];
+//     for (var key in obj) {
+//         if (obj.hasOwnProperty(key)) {
+//             parts.push(encodeURIComponent(key) + '=' + encodeURIComponent(obj[key]));
+//         }
+//     }
+//     return parts.join('&');
+// }
 
 
 
 
-
-var postUrl = 'http://127.0.0.1:8080/posttest';
-var params = obj_to_query({title:'test_title',kkk:'test param2'});
-var xhr = new XMLHttpRequest();
-xhr.open('POST', postUrl, true);
-xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-xhr.onreadystatechange = function() { 
-  console.log(xhr.readyState);
-  console.log(xhr.status);
-  console.log(xhr.responseText);
-  if (xhr.readyState == 4 && xhr.status == 200) {
-    console.log(xhr.responseText);
-  }
-};
-xhr.send(params);
+// // post teset
+// var postUrl = 'http://127.0.0.1:8080/posttest';
+// var params = obj_to_query({title:'test_title',kkk:'test param2'});
+// var xhr = new XMLHttpRequest();
+// xhr.open('POST', postUrl, true);
+// xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+// xhr.onreadystatechange = function() { 
+//   console.log(xhr.readyState);
+//   console.log(xhr.status);
+//   console.log(xhr.responseText);
+//   if (xhr.readyState == 4 && xhr.status == 200) {
+//     console.log(xhr.responseText);
+//   }
+// };
+// xhr.send(params);
 
 
 
@@ -62,7 +62,7 @@ xhr.send(params);
 function loadUI(){
   if(document.readyState!="complete")
   {
-    window.setTimeout('loadUI()', 1000);
+    window.setTimeout('loadUI()', 2000);
   }
   else
   {
@@ -71,6 +71,13 @@ function loadUI(){
     target.style.width=300+'px';
     //define bg picture
     document.getElementsByClassName('bgAllImage')[0].src='http://e.hiphotos.baidu.com/image/w%3D1920%3Bcrop%3D0%2C0%2C1920%2C1080/sign=c9e903afe51190ef01fb96d6fc2ba675/503d269759ee3d6d1927548941166d224e4ade8b.jpg';
+    //delete bg picture ctrl
+    var my = document.getElementsByClassName('wallpaper-ctrl')[0];
+    // console.log(my);
+    if(my!=null)
+    {
+      my.parentNode.removeChild(my);
+    }
   }
 }
 loadUI();
