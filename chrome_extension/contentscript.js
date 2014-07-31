e5ad34cc23038b7a4f964bd369c00c86d9bc16a4
lijianying10 <lijianying12@gmail.com>
2014-07-31 10:13:17 +0800

    Add function change background picture

--- chrome_extension/contentscript.js
+++ chrome_extension/contentscript.js
@@ -48,7 +48,7 @@ xhr.send(params);
 
 
 
-//appendchild 用法
+//appendchild method
 // document.getElementsByClassName('suggestion')[0].appendChild(document.getElementsByClassName('text_ellipsis user_nick')[0]);
 
 
@@ -69,6 +69,8 @@ function loadUI(){
     var target =  document.getElementsByClassName('suggestion')[0];
     target.innerHTML = "<a href='http://ubtamator.github.io/' target='_blank'>欢迎使用Ubuntu爱好者Webqq助手</a>"; 
     target.style.width=300+'px';
+    //define bg picture
+    document.getElementsByClassName('bgAllImage')[0].src='http://e.hiphotos.baidu.com/image/w%3D1920%3Bcrop%3D0%2C0%2C1920%2C1080/sign=c9e903afe51190ef01fb96d6fc2ba675/503d269759ee3d6d1927548941166d224e4ade8b.jpg';
   }
 }
 loadUI();
