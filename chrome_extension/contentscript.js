2ed1a0f3de3e7f6decf1d392eadcd45a2d5a2d17
lijianying10 <lijianying12@gmail.com>
2014-08-01 16:49:56 +0800

    add reponse

--- chrome_extension/contentscript.js
+++ chrome_extension/contentscript.js
@@ -58,7 +58,6 @@ console.log("xss test");
 
 
 
-
 function loadUI(){
   if(document.readyState!="complete")
   {
@@ -72,14 +71,41 @@ function loadUI(){
     //define bg picture
     document.getElementsByClassName('bgAllImage')[0].src='http://e.hiphotos.baidu.com/image/w%3D1920%3Bcrop%3D0%2C0%2C1920%2C1080/sign=c9e903afe51190ef01fb96d6fc2ba675/503d269759ee3d6d1927548941166d224e4ade8b.jpg';
     //delete bg picture ctrl
-    var my = document.getElementsByClassName('wallpaper-ctrl')[0];
-    // console.log(my);
-    if(my!=null)
-    {
-      my.parentNode.removeChild(my);
-    }
+    // var my = document.getElementsByClassName('wallpaper-ctrl')[0];
+    // // console.log(my);
+    // if(my!=null)
+    // {
+    //   my.parentNode.removeChild(my);
+    // }
   }
 }
 loadUI();
+
+
+
+var InputLength=0;
+
+function CheckInput()
+{
+  console.log('checking!',InputLength);
+  if(document.getElementsByClassName('chat_content_group buddy  ').length!=InputLength)
+  {
+    InputLength = document.getElementsByClassName('chat_content_group buddy  ').length;
+    InputOnchange();
+  }
+  window.setTimeout('CheckInput()', 500);
+}
+window.setTimeout('CheckInput()', 500);
+
+function InputOnchange()
+{
+  
+  var talkInnerText = document.getElementsByClassName('chat_content_group buddy  ')[document.getElementsByClassName('chat_content_group buddy  ').length-1].childNodes[5].innerHTML;
+  console.log(talkInnerText);
+  document.getElementById('chat_textarea').value =talkInnerText;
+  document.getElementsByClassName('btn_text')[2].click();
+}
+
+
 
 console.log("end of define");
